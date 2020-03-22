48a9fe69d6cb3cc889446737dcd946f34638e0d7
yejq <xiaoqzye@qq.com>
2020-03-22 13:27:50 +0000

    build packages for GNU/Linux host

---
+++ env-host.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# environment variables to compile for host
+
+export FI_RDIR="`/bin/pwd -P`"
+if [ "`echo -n ${FI_RDIR} | sed -r -e 's#\s+##'`" != "${FI_RDIR}" ] ; then
+	unset FI_RDIR # remove environment variable
+	echo "Error, white spaces not allowed in the path of fighters project" 1>&2
+else
+	unset FI_TCPREFIX
+	unset LD_LIBRARY_PATH
+	export FI_HOST_BUILD='true'
+	export FI_PREFIX=/opt/fight
+	export FI_HOST='x86_64-linux-gnu'
+	export FI_LDFLAGS="-Wl,-rpath=${FI_PREFIX}/lib"
+	export FI_CFLAGS='-Wall -O2 -fPIC -D_GNU_SOURCE'
+	export PATH=/usr/bin:/usr/sbin:/bin:/sbin
+fi
+
--- install.sh
+++ install.sh
@@ -4,13 +4,19 @@
 # Configure/Compile/Install applications for GNU/Linux
 # 2020/03/22
 
-unset LD_LIBRARY_PATH
-export MYI_TDIR="`/bin/pwd -P`"
-export PATH=/bin:/sbin:/usr/bin:/usr/sbin
+if [ -z "${FI_RDIR}" ] ; then
+	echo "Error, environment variable \`FI_RDIR not defined." 1>&2
+	exit 1
+fi
 
-DL_TARDIR="${MYI_TDIR}/tarballs"
+FI_BUILDDIR="${FI_RDIR}/build"
+DL_TARDIR="${FI_RDIR}/tarballs"
+SM_SRCDIR="${FI_RDIR}/submodules"
+export FI_CONFIGURED=".fighter.configured"
+export FI_COMPILED=".fighter.compiled"
+export FI_INSTALLED=".fighter.installed"
 # create directory to store the source tarballs
-mkdir -p "${DL_TARDIR}"
+[ ! -e "${DL_TARDIR}" ] && mkdir -v "${DL_TARDIR}"
 
 VAL_SHA256=""
 function get_sha256 {
@@ -62,11 +68,76 @@ function download_source {
 	fi
 	validate_sha256 "$2" "$3" || return 4
 	echo "downloaded source tarball: \"$2\""
-	chmod 444 "$2"
 	return 0
 }
 
-GNU_URL='http://mirrors.ustc.edu.cn/gnu'
-download_source "${GNU_URL}/make" 'make-4.3.tar.gz' 'e05fdde47c5f7ca45cb697e973894ff4f5d79e13b750ed57d7b66d8defc78e19'
-download_source "${GNU_URL}/bash" 'bash-5.0.tar.gz' 'b4a80f2ac66170b2913efbfb9f2594f1f76c7b1afd11f799e22035d63077fb4d'
+function comple_package {
+	local srcDir=""
+	local pkgName="$1"
+	cd "${FI_BUILDDIR}" || return $? # goto project build directory
+
+	local preTAG=".${pkgName}.prepared"
+	# determine the package directory name
+	if [ -d "${SM_SRCDIR}/${pkgName}" ] ; then
+		srcDir="${pkgName}"
+		if [ ! -e "${preTAG}" ] ; then
+			rm -rf "${srcDir}" ; sync
+			echo "Copy source files: \"${srcDir}\"..."
+			cp -r -p -P "${SM_SRCDIR}/${pkgName}" "${srcDir}" && touch "${preTAG}"
+		fi
+	elif [ ! -f "${DL_TARDIR}/${pkgName}" ] ; then
+		echo "Error, package not found: \"${pkgName}\"" 1>&2
+		return 1
+	else
+		srcDir="${pkgName%.tar*}"
+		if [ -z "${srcDir}" -o "${srcDir}" = "${pkgName}" ] ; then
+			echo "Error, invalid package: \"${pkgName}\"" 1>&2
+			return 2
+		fi
+		preTAG=".${srcDir}.prepared"
+		if [ ! -e "${preTAG}" ] ; then
+			rm -rf "${srcDir}" ; sync
+			echo "Extracting package: \"${pkgName}\"..."
+			tar -axf "${DL_TARDIR}/${pkgName}" && touch "${preTAG}"
+		fi
+	fi
+
+	cd "${srcDir}" || return $?
+	local builds="../build-${srcDir}.sh"
+	if [ ! -e "${builds}" ] ; then
+		echo "Error, build script \"${builds}\" not found." 1>&2
+		return 3
+	fi
+
+	if bash "${builds}" ; then
+		echo "Successfully compiled and installed \"${pkgName}\""
+		return 0
+	fi
+	echo "Error, failed to compile and install \"${pkgName}\"" 1>&2
+	return $?
+}
+
+function download_packages {
+	local retv=0
+	local GNU_URL='http://mirrors.ustc.edu.cn/gnu'
+
+	download_source "${GNU_URL}/make" 'make-4.3.tar.gz' \
+		'e05fdde47c5f7ca45cb697e973894ff4f5d79e13b750ed57d7b66d8defc78e19'
+	retv=$? ; [ ${retv} -ne 0 ] && return ${retv}
+
+	download_source "${GNU_URL}/bash" 'bash-5.0.tar.gz' \
+		'b4a80f2ac66170b2913efbfb9f2594f1f76c7b1afd11f799e22035d63077fb4d'
+	retv=$? ; [ ${retv} -ne 0 ] && return ${retv}
+
+	download_source "http://www.oberhumer.com/opensource/lzo/download" "lzo-2.10.tar.gz" \
+		'c0f892943208266f9b6543b3ae308fab6284c5c90e627931446fb49b4221a072'
+	retv=$? ; [ ${retv} -ne 0 ] && return ${retv}
+
+	return 0
+}
+
+download_packages || exit $?
+comple_package 'make-4.3.tar.gz' || exit $?
+comple_package 'bash-5.0.tar.gz' || exit $?
+comple_package 'lzo-2.10.tar.gz' || exit $?
 
