82c19d7a405a6d49611cecbd34526dd8eb80baed
yejq <xiaoqzye@qq.com>
2020-03-23 22:46:31 +0800

    import patch file from debian for lzo2 library

--- install.sh
+++ install.sh
@@ -10,13 +10,14 @@ if [ -z "${FI_RDIR}" ] ; then
 fi
 
 FI_BUILDDIR="${FI_RDIR}/build"
-DL_TARDIR="${FI_RDIR}/tarballs"
-SM_SRCDIR="${FI_RDIR}/submodules"
+FI_DL_DIR="${FI_RDIR}/tarballs"
+FI_SM_DIR="${FI_RDIR}/submodules"
+FI_PATCH_DIR="${FI_RDIR}/patches"
 export FI_CONFIGURED=".fighter.configured"
 export FI_COMPILED=".fighter.compiled"
 export FI_INSTALLED=".fighter.installed"
 # create directory to store the source tarballs
-[ ! -e "${DL_TARDIR}" ] && mkdir -v "${DL_TARDIR}"
+[ ! -e "${FI_DL_DIR}" ] && mkdir -v "${FI_DL_DIR}"
 
 VAL_SHA256=""
 function get_sha256 {
@@ -51,7 +52,7 @@ function validate_sha256 {
 
 # function to download source
 function download_source {
-	cd "${DL_TARDIR}" || return 1
+	cd "${FI_DL_DIR}" || return 1
 	if [ -f "$2" ] ; then
 		validate_sha256 "$2" "$3" || return 2
 		echo "already downloaded: \"$2\""
@@ -60,7 +61,7 @@ function download_source {
 
 	rm -rf "$2" ; sync
 	echo "downloading \"$2\"..."
-	wget "$1/$2"
+	wget -c --tries=32 -T 180 "$1/$2"
 	if [ $? -ne 0 ] ; then
 		echo "Error, failed to download \"$2\"." 1>&2
 		rm -rf "$2"
@@ -74,39 +75,48 @@ function download_source {
 function comple_package {
 	local srcDir=""
 	local pkgName="$1"
-	cd "${FI_BUILDDIR}" || return $? # goto project build directory
+	cd "${FI_BUILDDIR}" || return 1 # goto project build directory
 
 	local preTAG=".${pkgName}.prepared"
 	# determine the package directory name
-	if [ -d "${SM_SRCDIR}/${pkgName}" ] ; then
+	if [ -d "${FI_SM_DIR}/${pkgName}" ] ; then
 		srcDir="${pkgName}"
 		if [ ! -e "${preTAG}" ] ; then
 			rm -rf "${srcDir}" ; sync
 			echo "Copy source files: \"${srcDir}\"..."
-			cp -r -p -P "${SM_SRCDIR}/${pkgName}" "${srcDir}" && touch "${preTAG}"
+			cp -r -p -P "${FI_SM_DIR}/${pkgName}" "${srcDir}" && touch "${preTAG}"
 		fi
-	elif [ ! -f "${DL_TARDIR}/${pkgName}" ] ; then
+	elif [ ! -f "${FI_DL_DIR}/${pkgName}" ] ; then
 		echo "Error, package not found: \"${pkgName}\"" 1>&2
-		return 1
+		return 2
 	else
 		srcDir="${pkgName%.tar*}"
 		if [ -z "${srcDir}" -o "${srcDir}" = "${pkgName}" ] ; then
 			echo "Error, invalid package: \"${pkgName}\"" 1>&2
-			return 2
+			return 3
 		fi
 		preTAG=".${srcDir}.prepared"
 		if [ ! -e "${preTAG}" ] ; then
 			rm -rf "${srcDir}" ; sync
 			echo "Extracting package: \"${pkgName}\"..."
-			tar -axf "${DL_TARDIR}/${pkgName}" && touch "${preTAG}"
+			tar -axf "${FI_DL_DIR}/${pkgName}" && touch "${preTAG}"
 		fi
 	fi
 
-	cd "${srcDir}" || return $?
+	cd "${srcDir}" || return 5
+	local patdir="${FI_PATCH_DIR}/${srcDir}"
+	if [ -d "${patdir}" ] ; then
+		for patfile in "${patdir}"/*.patch ; do
+			[ ! -f "${patfile}" ] && continue
+			[ ! -e ".patched" ] && patch -Np1 -i "${patfile}"
+		done
+		unset patfile ; touch ".patched"
+	fi
+
 	local builds="../build-${srcDir}.sh"
 	if [ ! -e "${builds}" ] ; then
 		echo "Error, build script \"${builds}\" not found." 1>&2
-		return 3
+		return 6
 	fi
 
 	if bash "${builds}" ; then
@@ -114,7 +124,7 @@ function comple_package {
 		return 0
 	fi
 	echo "Error, failed to compile and install \"${pkgName}\"" 1>&2
-	return $?
+	return 7
 }
 
 function download_packages {
@@ -135,6 +145,10 @@ function download_packages {
 
 	return 0
 }
+
+if [ -n "$1" ] ; then
+	$@ ; exit $?
+fi
 
 download_packages || exit $?
 comple_package 'make-4.3.tar.gz' || exit $?
