25223e84266eae39e8fef519ed96363a821184ac
yejq <xiaoqzye@qq.com>
2020-04-05 12:12:23 +0800

    verbose built of lzo-2.10 library

--- build/build-lzo-2.10.sh
+++ build/build-lzo-2.10.sh
@@ -1,1 +1,23 @@
-build-common.sh
+#!/bin/bash
+
+retv=0
+if [ ! -e "${FI_CONFIGURED}" ] ; then
+	./configure --prefix=${FI_PREFIX} --host=${FI_HOST} CC=${FI_TCPREFIX}gcc CXX=${FI_TCPREFIX}g++ \
+		CFLAGS="${FI_CFLAGS}" LDFLAGS="${FI_LDFLAGS}" --disable-nls
+	retv=$?
+	[ ${retv} -eq 0 ] && touch "${FI_CONFIGURED}"
+fi
+[ ${retv} -ne 0 ] && exit ${retv}
+
+if [ ! -e "${FI_COMPILED}" ] ; then
+	make V=1 ; retv=$?
+	[ ${retv} -eq 0 ] && touch "${FI_COMPILED}"
+fi
+[ ${retv} -ne 0 ] && exit ${retv}
+
+if [ ! -e "${FI_INSTALLED}" ] ; then
+	make install ; retv=$?
+	[ ${retv} -eq 0 ] && touch "${FI_INSTALLED}"
+fi
+exit ${retv}
+
