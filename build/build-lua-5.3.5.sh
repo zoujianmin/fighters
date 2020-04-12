d39f869e23c0298a10f4be5c25940a16847b7a3e
yejq <xiaoqzye@qq.com>
2020-04-12 18:13:45 +0800

    dynamically set LUA_ROOT during compliation

--- build/build-lua-5.3.5.sh
+++ build/build-lua-5.3.5.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# check the installation prefix
+if [ -z "${FI_PREFIX}" ] ; then
+	echo "Error, \`FI_PREFIX not defined" 1>&2
+	exit 1
+fi
+
+lcfg='src/luaconf.h'
+if [ -n "`grep -e FI_PREFIX ${lcfg}`" ] ; then
+	sed -r -e "s#FI_PREFIX#${FI_PREFIX}/#g" -i "${lcfg}"
+fi
+
 retv=0
 if [ ! -e "${FI_COMPILED}" ] ; then
 	make ; retv=$?
--- submodules/lua-5.3.5/src/luaconf.h
+++ submodules/lua-5.3.5/src/luaconf.h
@@ -200,7 +200,7 @@
 
 #else			/* }{ */
 
-#define LUA_ROOT	"/usr/local/"
+#define LUA_ROOT	"FI_PREFIX"
 #define LUA_LDIR	LUA_ROOT "share/lua/"
 #define LUA_CDIR	LUA_ROOT "lib/lua/"
 #define LUA_PATH_DEFAULT  \
