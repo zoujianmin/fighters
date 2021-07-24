58dc51b390cc97bcb87368a2a6ad9018bf07061c
yejq <xiaoqzye@qq.com>
2021-07-24 14:32:28 +0800

    build lzo-2.10 & lua-5.3.6

---
+++ common/sources.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Copyright 2021 Ye Holmes <yeholmes@outlook.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+lua53_config() {
+    patch -Np1 -i ${TOPBDIR}/patches/lua-5.3.6/0001-modify-Makefile-to-build-for-GNU-Linux.patch
+    return $?
+}
+
+lua53_build() {
+    make FTC_PREFIX=${FTC_PREFIX} FTC_CFLAGS="${FTC_CFLAGS}" -j1
+    return $?
+}
+
+fighter_clean() {
+    make -j1 clean
+    return $?
+}
+
+liblzo2_config() {
+    ./configure --prefix=/usr --host=${FTC_HOST} CC=${FTC_CC} CXX=${FTC_CXX} \
+		CFLAGS="${FTC_FLAGS}" --enable-shared=no --enable-static=yes
+    return $?
+}
+
+liblzo2_build() {
+    make V=1 -j1 || return 1
+    make DESTDIR="${FSTAGING_DIR}" install
+    return $?
+}
+
+register_source 'lua-5.3.6.tar.gz' \
+    lua53_config lua53_build fighter_clean
+register_source 'lzo-2.10.tar.gz' \
+    liblzo2_config liblzo2_build fighter_clean
---
+++ x86_64/toolchain.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Copyright 2021 Ye Holmes <yeholmes@outlook.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+declare -r FTC_HOST=x86_64-linux-gnu
+declare -r FTC_PREFIX="${FTC_HOST}-"
+declare -r FTC_CC="${FTC_PREFIX}gcc"
+declare -r FTC_CXX="${FTC_PREFIX}g++"
+declare -r FTC_STRIP="${FTC_PREFIX}strip"
+declare -r FTC_AR="${FTC_PREFIX}ar"
+declare -r FTC_RANLIB="${FTC_PREFIX}ranlib"
+declare -r FTC_LD="${FTC_PREFIX}ld"
+
+declare -r FTC_FLAGS="-Wall -fPIC -O2 -ggdb -D_GNU_SOURCE"
+declare -r FTC_CFLAGS="${FTC_FLAGS}"
+declare -r FTC_CXXFLAGS="${FTC_FLAGS}"
