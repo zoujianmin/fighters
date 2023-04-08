b78f8e7007a73f38dd636da603678da97113ac49
yejq <xiaoqzye@qq.com>
2023-04-08 16:47:42 +0800

    rbtree: remove rcu related functions

--- rbtree/rbtree.h
+++ rbtree/rbtree.h
@@ -88,8 +88,6 @@ extern struct rb_node *rb_next_postorder(const struct rb_node *);
 /* Fast replacement of a single node without remove/rebalance/add/rebalance */
 extern void rb_replace_node(struct rb_node *victim, struct rb_node *new,
 			    struct rb_root *root);
-extern void rb_replace_node_rcu(struct rb_node *victim, struct rb_node *new,
-				struct rb_root *root);
 
 static inline void rb_link_node(struct rb_node *node, struct rb_node *parent,
 				struct rb_node **rb_link)
@@ -98,15 +96,6 @@ static inline void rb_link_node(struct rb_node *node, struct rb_node *parent,
 	node->rb_left = node->rb_right = NULL;
 
 	*rb_link = node;
-}
-
-static inline void rb_link_node_rcu(struct rb_node *node, struct rb_node *parent,
-				    struct rb_node **rb_link)
-{
-	node->__rb_parent_color = (unsigned long)parent;
-	node->rb_left = node->rb_right = NULL;
-
-	rcu_assign_pointer(*rb_link, node);
 }
 
 #define rb_entry_safe(ptr, type, member) \
